9c17ea832f61a1ff6ff75fd8827bec69b42a3be9
Pavel Hristov <pavelh@abv.bg>
2014-07-31 17:53:24 +0300

    Added Proxy.

---
+++ Proxy/ProxyExample.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Proxy Design Pattern.
+    /// </summary>
+    static class ProxyExample
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Proxy' class
+    /// </summary>
+    class Proxy : Subject
+    {
+        private RealSubject realSubject;
+
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+
+            realSubject.Request();
+        }
+    }
+}
