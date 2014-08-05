b2641a6afc4b324a234d6c21f4a82e9f20c21086
Pavel Hristov <pavelh@abv.bg>
2014-08-05 20:03:11 +0300

    Factory Method - Documents.csproj and Singleton - Load Balancer.csproj

---
+++ Singleton - Load Balancer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton___Load_Balancer
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class LoadBalancer
+    {
+        private static LoadBalancer instance;
+        private List<string> servers = new List<string>();
+        private Random random = new Random();
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            servers.Add("ServerI");
+            servers.Add("ServerII");
+            servers.Add("ServerIII");
+            servers.Add("ServerIV");
+            servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        // Simple, but effective random load balancer
+        public string Server
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r].ToString();
+            }
+        }
+    }
+}
---
+++ Singleton/SingletonExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Singleton
+{
+    public static class SingletonExample
+    {
+        static void Main()
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton instance;
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
