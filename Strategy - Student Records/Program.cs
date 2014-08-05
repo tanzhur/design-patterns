516b87dd8d105687b5555ec81201447bef4783f0
Pavel Hristov <pavelh@abv.bg>
2014-08-05 14:18:07 +0300

    Added strategy and abstract factory.

---
+++ Strategy - Student Records/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy___Student_Records
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Two contexts following different strategies
+            var studentRecords = new SortedList();
+
+            studentRecords.Add("Samual");
+            studentRecords.Add("Jimmy");
+            studentRecords.Add("Sandra");
+            studentRecords.Add("Vivek");
+            studentRecords.Add("Anna");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    abstract class SortStrategy
+    {
+        public abstract void Sort(List<string> list);
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort(); // Default is Quicksort
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort(); not-implemented
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.MergeSort(); not-implemented
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class SortedList
+    {
+        private List<string> list = new List<string>();
+        private SortStrategy sortStrategy;
+
+        public void SetSortStrategy(SortStrategy sortStrategy)
+        {
+            this.sortStrategy = sortStrategy;
+        }
+
+        public void Add(string name)
+        {
+            list.Add(name);
+        }
+
+        public void Sort()
+        {
+            sortStrategy.Sort(list);
+
+            foreach (string name in list)
+            {
+                Console.WriteLine(" " + name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Strategy/StrategyExample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Strategy
+{
+    public static class StrategyExample
+    {
+        static void Main()
+        {
+            Context context;
+
+            // Three contexts following different strategies
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+        }
+    }
+
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("Called ConcreteStrategyA.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("Called ConcreteStrategyB.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("Called ConcreteStrategyC.AlgorithmInterface()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class Context
+    {
+        private Strategy stradegy;
+
+        public Context(Strategy stradegy)
+        {
+            this.stradegy = stradegy;
+        }
+
+        public void ContextInterface()
+        {
+            stradegy.AlgorithmInterface();
+        }
+    }
+}
